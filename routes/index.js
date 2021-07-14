9923d51bd7c1002114a30435bf6df6f6bec34656
Zak Stamps <theoneandonlyzako@gmail.com>
2021-07-13 23:35:12 -0700

    Users can create, read, update, and delete data for a pizza in the database

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+// Inport All of the api routes from /api/index.js (no need for index.js though since its implied)
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html/html-routes');
+
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>😝 404 Error!</h1>');
+});
+
+module.exports = router;
